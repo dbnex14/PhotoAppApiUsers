e149dd41903b4989ebfb77e506c1fa871680419f
dino <mypascal2000@gmail.com>
2022-09-23 13:09:50 -0700

    Init eureka client microservice app

---
+++ src/test/java/io/dino/learning/photoappapiusers/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dino.learning.photoappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
